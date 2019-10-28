9e4f0a5b0ee5f7877f28bae9c9efbdfee0f215f1
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-28 11:37:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,4 +11,18 @@ def prime?(num)
   end
 end
 
-puts Benchmark.measure { BigMath.PI(10_000) }
+Benchmark.bm do |bm|
+  # joining an array of strings
+  bm.report do
+    iterations.times do
+      ["The", "current", "time", "is", Time.now].join(" ")
+    end
+  end
+
+  # using string interpolation
+  bm.report do
+    iterations.times do
+      "The current time is #{Time.now}"
+    end
+  end
+end
